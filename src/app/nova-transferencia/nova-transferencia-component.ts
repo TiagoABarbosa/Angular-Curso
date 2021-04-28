761aa18dc10b3e8d2f5fc9b3188e69f1b749d64a
Tiago Barbosa <82111560+TiagoABarbosa@users.noreply.github.com>
2021-04-28 00:14:44 -0300

    commit

---
+++ src/app/nova-transferencia/nova-transferencia-component.ts
@@ -0,0 +1,28 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+//import * as EventEmitter from 'node:events';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = null;
+    this.destino = null;
+  }
+
+}
