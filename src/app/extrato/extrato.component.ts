3efb1ad2aa1c4c9867a18a6b8855a3cb8a577d80
Tiago Barbosa <82111560+TiagoABarbosa@users.noreply.github.com>
2021-04-29 01:08:35 -0300

    Update 29/04

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -3,14 +3,12 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.scss']
+  styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
-  @Input() transferencia: any;
+  @Input() transferencias: any[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
